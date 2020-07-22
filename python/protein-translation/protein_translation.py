4661c5a3235edb4dd5f8ec2aa02d854b78453ec1
aleksandar-had <50676450+al-hadzhi@users.noreply.github.com>
2020-07-22 10:41:24 +0200

    protein_translation with regex

---
+++ python/protein-translation/protein_translation.py
@@ -0,0 +1,31 @@
+import re
+
+codon_to_protein = {
+    "AUG": "Methionine",
+    "UUU": "Phenylalanine",
+    "UUC": "Phenylalanine",
+    "UUA": "Leucine",
+    "UUG": "Leucine",
+    "UCU": "Serine",
+    "UCC": "Serine",
+    "UCA": "Serine",
+    "UCG": "Serine",
+    "UAU": "Tyrosine",
+    "UAC": "Tyrosine",
+    "UGU": "Cysteine",
+    "UGC": "Cysteine",
+    "UGG": "Tryptophan",
+    "UAA": "STOP",
+    "UAG": "STOP",
+    "UGA": "STOP",
+}
+
+
+def proteins(strand):
+    codons = re.findall(r".{1,3}", strand)
+    translation = []
+    for codon in codons:
+        if codon_to_protein.get(codon) == "STOP":
+            break
+        translation.append(codon_to_protein[codon])
+    return translation
