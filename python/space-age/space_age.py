3ae39508d06692328be9a2ac8adc5861729ce819
aleksandar-had <50676450+al-hadzhi@users.noreply.github.com>
2020-10-11 00:06:18 +0200

    space age python clean

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -1,4 +1,14 @@
 class SpaceAge:
+    orbital_periods = {
+        "mercury": 0.2408467,
+        "venus": 0.61519726,
+        "mars": 1.8808158,
+        "jupiter": 11.862615,
+        "saturn": 29.447498,
+        "uranus": 84.016846,
+        "neptune": 164.79132,
+    }
+
     def __init__(self, seconds):
         self.age = seconds
         self.age_on_earth = seconds / 31557660
@@ -7,22 +17,22 @@ class SpaceAge:
         return round(self.age_on_earth, 2)
 
     def on_mercury(self):
-        return round(self.age_on_earth / 0.2408467, 2)
+        return round(self.age_on_earth / self.orbital_periods["mercury"], 2)
 
     def on_venus(self):
-        return round(self.age_on_earth / 0.61519726, 2)
+        return round(self.age_on_earth / self.orbital_periods["venus"], 2)
 
     def on_mars(self):
-        return round(self.age_on_earth / 1.8808158, 2)
+        return round(self.age_on_earth / self.orbital_periods["mars"], 2)
 
     def on_jupiter(self):
-        return round(self.age_on_earth / 11.862615, 2)
+        return round(self.age_on_earth / self.orbital_periods["jupiter"], 2)
 
     def on_saturn(self):
-        return round(self.age_on_earth / 29.447498, 2)
+        return round(self.age_on_earth / self.orbital_periods["saturn"], 2)
 
     def on_uranus(self):
-        return round(self.age_on_earth / 84.016846, 2)
+        return round(self.age_on_earth / self.orbital_periods["uranus"], 2)
 
     def on_neptune(self):
         return round(self.age_on_earth / 164.79132, 2)
